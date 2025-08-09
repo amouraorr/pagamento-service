f125bb975dc7d1feb22e49017414f08f0fa95ce0
Alex Mourão <amouraorr@yahoo.com.br>
2025-08-09 14:28:05 -0300

    Ajustes nescessários

--- src/main/java/com/fiap/pagamento/gateway/impl/GatewayPagamentoAdapter.java
+++ src/main/java/com/fiap/pagamento/gateway/impl/GatewayPagamentoAdapter.java
@@ -9,11 +9,9 @@ public class GatewayPagamentoAdapter implements GatewayPagamentoPort {
 
     @Override
     public String processarPagamentoExterno(Pagamento pagamento) {
-        // Recusa se o número do cartão for "0000000000000000"
         if ("0000000000000000".equals(pagamento.getNumeroCartao())) {
             return "RECUSADO";
         }
-        // Aprova se valor <= 1000, recusa acima disso (ajuste conforme sua regra)
         if (pagamento.getValor() > 1000) {
             return "RECUSADO";
         }
